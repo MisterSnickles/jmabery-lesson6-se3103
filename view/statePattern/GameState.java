833d1f42d6bf8bc69944e7dfa47309e2156ee9d6
Jonathan Mabery <papasnickles31@gmail.com>
2026-03-26 22:44:41 -0500

    gamestate

---
+++ view/statePattern/GameState.java
@@ -0,0 +1,11 @@
+package view.statePattern;
+
+import java.awt.Graphics2D;
+import view.AppWindow;
+
+
+public interface GameState {
+    void goNext(AppWindow appWindow);
+    void updateWindow();
+    void updateCanvas(Graphics2D g2);
+}
